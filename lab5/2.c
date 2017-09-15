00e69d12708a17e23e966a5473d735e9856622df
aditya1904 <adityamalu1@gmail.com>
2017-09-15 23:41:53 +0530

    2nd done!

---
+++ lab5/2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+	if(argc != 2){
+		printf("Usage : %s <filename>\n", argv[0]);
+		exit(0);
+	}
+	char *filename = argv[1];
+	char buffer1[] = "abcde", buffer2[] = "12345";
+	int fd1, fd2, n;
+	fd1 = open(filename , O_CREAT | O_WRONLY | O_TRUNC);
+	if(fd1 == -1){
+		perror("Error ");
+		exit(1);
+	}
+
+	printf("Writing \"%s\" to file \"%s\" in parent process...\n", buffer1, filename);
+	write(fd1, buffer1, strlen(buffer1));
+
+	int pid = fork();
+
+	if(!pid){
+		printf("Writing \"%s\" to file \"%s\" in child process...\n", buffer2, filename);
+		write(fd1, buffer2, strlen(buffer2));
+		close(fd1);
+	} else {
+		close(fd1);
+	}
+
+	return 0;
+}
