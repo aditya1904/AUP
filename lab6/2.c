8e23988ced0536c3f89e9bc861852c7172910964
aditya1904 <adityamalu1@gmail.com>
2017-09-29 22:30:40 +0530

    2.c done.

--- lab6/2.c
+++ lab6/2.c
@@ -1,37 +1,41 @@
-#include<pthread.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<errno.h>
-#include<unistd.h>
+#define _GNU_SOURCE             /* See feature_test_macros(7) */
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
 
-void* hello(void *cpu_id){
-	int cpu = *(int *)cpu_id;
+void* hello(void *cpu_set){
 	pthread_t thread = pthread_self();
-	cpu_set_t cpuset;
-	CPU_ZERO(&cpuset);
-	CPU_SET(cpu, &cpuset);
-	
-	int s = pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+	cpu_set_t *cpuset = (cpu_set_t *)cpu_set;
+	int s = pthread_setaffinity_np(thread, sizeof(cpu_set_t), cpuset);
 	if (s != 0)
-		printf("Error for CPU %d\n", cpu);
+		printf("Error for CPU");
 }
 
 int main(){
 	long int n_cpu =  sysconf(_SC_NPROCESSORS_ONLN);
-	int j;
+	printf("No. of CPUs -> %ld\n", n_cpu);
+	int j, s;
 	pthread_t threads[n_cpu];
 	cpu_set_t cpuset;
 	CPU_ZERO(&cpuset);
 	for (j = 0; j < n_cpu; j++){
 		CPU_SET(j, &cpuset);
 	}
+	printf("Attaching a thread to a CPU...\n");
 	for (j = 0; j < n_cpu; j++){
-		pthread_create(&threads[j], NULL, hello, &j);
+		pthread_create(&threads[j], NULL, hello, &cpuset);
 	}
-	printf("Set returned by pthread_getaffinity_np() contained:\n");
-	for (j = 0; j < CPU_SETSIZE; j++){
-		if (CPU_ISSET(j, &cpuset))
-			printf("    CPU %d\n", j);
+
+	printf("Checking for threads, if attached to a CPU...\n");
+	for (j = 0; j < n_cpu; j++){
+		s = pthread_getaffinity_np(threads[j], sizeof(cpu_set_t), &cpuset);
+		printf("Thread %d -> %s\n", j, !s?"true":"false");
 	}
-	exit(EXIT_SUCCESS);	
+
+	for(j = 0; j < n_cpu; j++) {
+		pthread_join(threads[j], NULL);
+	}
+	exit(EXIT_SUCCESS);
 }
