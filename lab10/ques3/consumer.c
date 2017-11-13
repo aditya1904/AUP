bee8f0771144700e407fd8fc6a46f68b02e88adc
Akash Sarda <akashsarda3@gmail.com>
2017-11-13 13:58:17 +0530

    3 not done

---
+++ lab10/ques3/consumer.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/ipc.h>
+#include<sys/types.h>
+#include<sys/shm.h>
+#include<sys/sem.h>
+int main()
+{
+	key_t key = 5678, skey = IPC_PRIVATE;
+	int i;	
+	int shmid = shmget(key,10 , IPC_CREAT | 0666), semid = semget(skey, 1, IPC_CREAT | IPC_EXCL | 0666);
+	char *shm = shmat(shmid, NULL, 0), *ptr;
+	struct sembuf sb;
+	ptr = shm;
+	if(semid >= 0){
+
+/*		union semun{
+			int val;
+			struct semid_dss *buf;
+			short *array;
+		}arg;
+
+		arg.val = 0;
+		semctl(semid, 0, SETVAL, arg); */
+		i = 0;
+		printf("seems consumer is ready too.\n");
+		while(1){
+			if(i == 9){
+				i = 0;
+				ptr = shm;
+			}
+
+			sb.sem_num = 0;
+			sb.sem_op = -1;			
+			semop(semid, &sb, 1);
+			printf("got hold of the semaphore.\n");
+			if(*ptr == 'c'){
+				// buffer is empty.
+				sb.sem_num = 0;
+				sb.sem_op = 1;			
+				semop(semid, &sb, 1);
+				continue;
+			}else{
+				// get the semaphore
+				*ptr = 'c';
+				ptr++;
+				i = (i + 1) % 10;
+				// release the semaphore
+			}
+			
+			sb.sem_num = 0;
+			sb.sem_op = 1;			
+			semop(semid, &sb, 1);
+			
+		}
+	
+	}
+	shm[10] = NULL;
+	exit(0);
+}
---
+++ lab10/ques3/producer.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/ipc.h>
+#include<sys/types.h>
+#include<sys/shm.h>
+#include<sys/sem.h>
+int main()
+{
+	key_t key = 5678, skey = IPC_PRIVATE;
+	int i;	
+	int shmid = shmget(key,10 , IPC_CREAT | 0666), semid = semget(skey, 1, IPC_CREAT | IPC_EXCL | 0666);
+	char *shm = shmat(shmid, NULL, 0), *ptr;
+	struct sembuf sb;
+
+	ptr = shm;	
+	if(semid >= 0){
+
+		union semun{
+			int val;
+			struct semid_dss *buf;
+			short *array;
+		}arg;
+
+		arg.val = 0;
+		semctl(semid, 0, SETVAL, arg); 
+		i = 0;
+		printf("seems, that semaphore has been made.\n");
+		while(1){
+			if(i == 9){
+				ptr = shm;
+				i = 0;
+			}
+			sb.sem_num = 0;
+			sb.sem_op = -1;			
+			semop(semid, &sb, 1);	
+			printf("semaphore is with me.\n");
+			if(*ptr == 'p'){
+				// buffer is full.
+				sb.sem_num = 0;
+				sb.sem_op = 1;			
+				semop(semid, &sb, 1);	
+				continue;
+			}else{
+				printf("producing.\n");
+				*ptr = 'p';
+				ptr++;
+				i = (i + 1) % 10;
+			}
+			sb.sem_num = 0;
+			sb.sem_op = 1;			
+			semop(semid, &sb, 1);	
+		}
+	}
+	else{
+		printf("Semaphore error.\n");
+	}
+	shm[10] = NULL;
+	exit(0);
+}
