0b3143e578e8c81ce0d7f9bff2754a3290a22f0a
Akash Sarda <akashsarda3@gmail.com>
2017-11-14 10:17:02 +0530

    9

--- lab9/3.c
+++ lab9/3.c
@@ -6,6 +6,14 @@
 
 static jmp_buf env;
 
+int abs(int x){
+	if ( x > 0){
+		return x;
+	}else{
+		return -1 * x;
+	}
+}
+
 int sig_alrm(int signo){
 	longjmp(env, 1);
 }
@@ -13,15 +21,26 @@ int sig_alrm(int signo){
 unsigned int mysleep(unsigned int secs){
 	int time_left;
 	time_left = alarm(0); /* checking if any previously set alarm function has some unslept seconds left */
+	void *func = signal(SIGALRM, sig_alrm);
 	printf("remaining time is %d\n", time_left);
-	if(setjmp(env) == 0){
-		alarm(secs - time_left); /* adding that time for user set value */
+	if(time_left > secs){
+		if(setjmp(env) == 0){
+			alarm(secs); /* adding that time for user set value */
+			pause();
+		}
+		alarm(time_left - secs);
+	}else{
+		alarm(time_left);
 		pause();
+		if(setjmp(env) == 0){
+			alarm((secs - time_left));  /* adding that time for user set value */
+		}
 	}
+	signal(SIGALRM, func); 
 }
 
 int main(){
-	alarm(3);
-	mysleep(4);
+	alarm(9);
+//a	mysleep(4);
 }
 
